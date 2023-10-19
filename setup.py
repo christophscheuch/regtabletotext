b78640b75ad6a063338f4ad480c54459d7497fc5
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2023-10-19 18:45:04 +0200

    Renamed package to regtabletotext

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open('HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
 setup_args = dict(
-    name='regtableformatter',
+    name='regtabletotext',
     version='0.0.1',
-    description='Helpers to format regression output',
+    description='Helpers to print regression output as well-formated text',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
